cb8c12fe06a284751055e757259bca906099a6cb
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-01-06 16:30:40 +0000

    Initial setup

---
+++ src/om_pouch/core.cljs
@@ -0,0 +1,12 @@
+(ns om-tutorial.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]
+            cljsjs.pouchdb))
+
+(enable-console-print!)
+
+(defonce db (js/PouchDB. "om-pouch"))
+
+(println "Hello world!")
+
