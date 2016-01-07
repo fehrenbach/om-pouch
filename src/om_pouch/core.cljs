584a61e09245e28edf58d8ad17a2bf379404ebde
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2016-01-07 13:02:55 +0000

    Putting fetching in read works, but overfetches massively

--- src/om_pouch/core.cljs
+++ src/om_pouch/core.cljs
@@ -9,84 +9,46 @@
 
 (defonce db (js/PouchDB. "om-pouch"))
 
-(comment          :pouch/by-id {"2" {:_id "2" :name "Joe" :age 33 :married false}
-                                "3" {:_id "3" :name "Paul" :age 45 :married true :married-to [:pouch/by-id "1"]}
-                                "4" {:_id "4" :name "Bob" :age 55 :married true :married-to [[:pouch/by-id "1"]
-                                                                                             [:pouch/by-id "3"]]}
-                                "5" {:_id "5" :name "Jim" :age 35 :married-to [:pouch/by-id "4"]}})
-
 (defonce app-state
   (atom {:window/size [1920 1200]}))
 
+(declare reconciler)
+
 (comment
-  (.put db (clj->js {:_id "6" :type "person"
-                     :name "Ahmad" :age 27
-                     :married-to ["1"]}))
-  
   (.put db (clj->js {:_id "1" :type "person"
                      :name "Sally" :age 22
                      :married-to ["6"]}))
-  
+
+  (.put db (clj->js {:_id "2" :type "person"
+                     :name "Bob" :age 33
+                     :married-to ["3" "4"]}))
+
+  (.put db (clj->js {:_id "3" :type "person"
+                     :name "Jim" :age 32
+                     :married-to ["2" "4"]}))
+
+  (.put db (clj->js {:_id "4" :type "person"
+                     :name "Robert" :age 28
+                     :married-to ["2" "3"]}))
+
+  (.put db (clj->js {:_id "5" :type "person"
+                     :name "Rey" :age 23
+                     :married-to []}))
+
+  (.put db (clj->js {:_id "6" :type "person"
+                     :name "Ahmad" :age 27
+                     :married-to ["1"]}))
   )
 
 
-
-(defmulti read om/dispatch)
-
-(defmethod read :window/size
-  [{:keys [state]} key params]
-  {:value (:window/size @state)})
-
-(defmethod read :pouch/by-id
-  [{:keys [ast parser state query] :as env} key params]
-  (.log js/console "read " (str (:key ast)))
-  (if-let [doc (get-in @state (:key ast))]
-    (let [_ (.log js/console "found " (str (:key ast)) " in state, making recursive call")
-          pouch (parser (assoc env :context doc) query :pouch)
-          value (parser (assoc env :context doc) query)]
-      (do
-        (.log js/console "recursive call value: " (str value))
-        (.log js/console "recursive call pouch: " (str pouch))
-        {:value value}))
-    (let [_ (.log js/console "did not find " (str (:key ast)) " in state, returning :loading and :pouch query " (str (first (:children (om/query->ast [(:key ast)])))))]
-      {:value :loading
-       ;; At some point we have to flatten the query to only retrieve docs.
-       ;; Not sure this is where we do it.
-       :pouch (first (:children (om/query->ast [(:key ast)])))})))
-
-(defmethod read :default
-  [{:keys [ast context parser query] :as env} key params]
-  (case (:type ast)
-    :prop
-    {:value (get context key)}
-
-    :join
-    (let [join-key (get context (:key ast))
-          ;; Om does not distinguish 1:1 1:N N:1 N:M joins
-          ;; We don't really want to call `the` all the time, so we try to autodetect it.
-          ;; BUG Unfortunately, this use of om/ident? is not safe (we apply it to data from the state, not a query.
-          ;; TODO Make this configurable (pass a parameter to the join?) and possibly remove autodetection / warn.
-          ;; one-to vs many-to
-          join-keys (if (om/ident? join-key) [join-key] join-key)
-          result (mapv (fn [join-key]
-                         ;; TODO What should the context be in env?
-                         (second (first (parser env [{join-key query}]))))
-                       join-keys)
-          ;; to-one vs to-many
-          result (if (= :pouch/by-id (first join-key)) (nth result 0) result)]
-      {:value result})))
-
 (defn postprocess-doc [doc]
+  (.log js/console "postprocess " (str doc))
   (-> doc
       (update :married-to (partial mapv (partial vector :pouch/by-id)))))
 
-(declare reconciler)
-
-(defn send
-  [{:keys [pouch] :as remote-queries} cb]
-  (.log js/console "send query:" (str pouch))
-  ;; We might need to do some normalization or something, split doc fetches from view fetches or whatnot.
-  (let [ids (map second pouch)]
+;; TODO should probably use a channel, deduplicate and group for bulk fetching
+(defn fetch-doc! [id]
+  (let [ids [id]]
     (.log js/console "fetch docs: " (str ids))
     (.allDocs
      db
@@ -112,17 +74,59 @@
            ;; Doesn't seem to work.
            (om/force-root-render! reconciler)))))))
 
+(defmulti read om/dispatch)
+
+(defmethod read :window/size
+  [{:keys [state]} key params]
+  {:value (:window/size @state)})
+
+(defmethod read :pouch/by-id
+  [{:keys [ast parser state query] :as env} key params]
+  ;; (.log js/console "read " (str (:key ast)))
+  (if-let [doc (get-in @state (:key ast))]
+    (let [;; _ (.log js/console "found " (str (:key ast)) " in state, making recursive call")
+          value (parser (assoc env :context doc) query)]
+      (do
+        ;; (.log js/console "recursive call value: " (str value))
+        {:value value}))
+    (let [;; _ (.log js/console "did not find " (str (:key ast)) " in state, returning :loading and firing off external load")
+          _ (fetch-doc! (second (:key ast)))]
+      {:value :loading})))
+
+(defmethod read :default
+  [{:keys [ast context parser query] :as env} key params]
+  (case (:type ast)
+    :prop
+    {:value (get context key)}
+
+    :join
+    (let [join-key (get context (:key ast))
+          ;; Om does not distinguish 1:1 1:N N:1 N:M joins
+          ;; We don't really want to call `the` all the time, so we try to autodetect it.
+          ;; BUG Unfortunately, this use of om/ident? is not safe (we apply it to data from the state, not a query.
+          ;; TODO Make this configurable (pass a parameter to the join?) and possibly remove autodetection / warn.
+          ;; one-to vs many-to
+          join-keys (if (om/ident? join-key) [join-key] join-key)
+          result (mapv (fn [join-key]
+                         ;; TODO What should the context be in env?
+                         (second (first (parser env [{join-key query}]))))
+                       join-keys)
+          ;; to-one vs to-many
+          result (if (= :pouch/by-id (first join-key)) (nth result 0) result)]
+      {:value result})))
+
 (def parser
   (om/parser {:read read}))
 
 (def reconciler
   (om/reconciler {:state app-state
-                  :parser parser
-                  :send send
-                  :remotes [:remote :pouch]}))
+                  :parser parser}))
 
 (def query [:window/size
-            {[:pouch/by-id "6"] [:name {:married-to [:name {:married-to [:name]}]}]}])
+            {[:pouch/by-id "3"] [:name {:married-to [:name {:married-to [:name]}]}]}
+            {[:pouch/by-id "4"] [:name {:married-to [:name {:married-to [:name]}]}]}
+            {[:pouch/by-id "6"] [:name {:married-to [:name {:married-to [:name]}]}]}
+            ])
 
 (defui RootView
   static om/IQuery
@@ -132,8 +136,15 @@
   Object
   (render [this]
           (let [props (om/props this)
-                six (get props [:pouch/by-id "6"])]
-            (dom/p nil (str six)))))
+                three (get props [:pouch/by-id "3"])
+                four (get props [:pouch/by-id "4"])
+                six (get props [:pouch/by-id "6"])
+                ]
+            (dom/div nil
+                     (dom/p nil "three" (str three))
+                     (dom/p nil "four" (str four))
+                     (dom/p nil "six" (str six))
+                     ))))
 
 (comment
   ;; ID not actually unique..
@@ -169,5 +180,3 @@
 
 (om/add-root! reconciler
               RootView (gdom/getElement "app"))
-
-(parser {:state app-state} query)
